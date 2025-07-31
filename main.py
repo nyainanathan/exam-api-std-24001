3c57ea7c421edf7a7137ac26a85e8ce701bc0159
nyainanathan <nyaina.nathan.1@gmail.com>
2025-07-31 08:31:06 +0300

    feat: question 2

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ app = FastAPI()
 def send_pong():
     return Response(content="pong" ,status_code=200,  media_type="text/plain")
 
+@app.get("/home")
+def welcome_home():
+    with open("welcome.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=200, media_type="text/html")
