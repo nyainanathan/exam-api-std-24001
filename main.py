99db9b9189250712d7ed6038e844dd8fd30bedfd
nyainanathan <nyaina.nathan.1@gmail.com>
2025-07-31 08:26:57 +0300

    feat: question 1

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
+
+@app.get("/ping")
+def send_pong():
+    return Response(content="pong" ,status_code=200,  media_type="text/plain")
+
