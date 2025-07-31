1a7ec637a89ed79338fa8e6a5e3c576ea93653f1
nyainanathan <nyaina.nathan.1@gmail.com>
2025-07-31 08:49:55 +0300

    feat: question 4

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import _pydatetime
+from _pydatetime import datetime
 from fastapi import FastAPI
+from pydantic_core.core_schema import DatetimeSchema
 from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 from pydantic import BaseModel
@@ -21,3 +24,19 @@ def catch_invalid_paths(full_path: str):
     with open("error.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=404, media_type="text/html")
+
+class PostPayload(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: str
+
+post_list : List[PostPayload] = []
+
+@app.post("/posts")
+def create_posts(new_posts: List[PostPayload]):
+    resp = []
+    post_list.extend(new_posts)
+    for post in post_list:
+        resp.append(post.model_dump())
+    return JSONResponse(content={"posts" : resp} , status_code=201)
