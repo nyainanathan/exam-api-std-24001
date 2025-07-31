febf175ca4d4b0c8f3039925db382c7c80e98b32
nyainanathan <nyaina.nathan.1@gmail.com>
2025-07-31 08:33:52 +0300

    feat: question 3

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ def welcome_home():
     with open("welcome.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
+
+@app.get("/{full_path:path}")
+def catch_invalid_paths(full_path: str):
+    with open("error.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=404, media_type="text/html")
